a2db574e3fb7f26b752133efce88cc8735d50716
Yu Wang <yuwang1@wharton.upenn.edu>
2018-03-28 19:30:26 -0400

    put scoring function in its own file, added hand comparisons

--- Hand.py
+++ Hand.py
@@ -5,76 +5,32 @@ Created on Tue Mar 27 18:36:12 2018
 @author: yuwan
 """
 
+import HandScorer as hs
+from functools import total_ordering
+    
 HAND_SIZE = 5
 suits = 'cdhs'
-ranks = 'AKQJT98765432'
-rankmap = {r: ranks.index(r) for r in ranks}
-
-handranks = {0: 'Straight Flush',
-             1: 'Four of a Kind',
-             2: 'Full House',
-             3: 'Flush',
-             4: 'Straight',
-             5: 'Three of a Kind',
-             6: 'Two Pair',
-             7: 'One Pair',
-             8: 'High Card'
-             }
-
-allHands = []
-temp = [''] * 5
-
-def addNextCard(hand):
-    global allHands
-    if len(hand)==5:
-        if hand != hand[0] * 5:
-            allHands.append(hand + 'o')
-        return
-    for card in ranks[ranks.index(hand[-1]):]:
-        addNextCard(hand + card)
-
-def addFlush(hand):
-    global allHands
-    if len(hand)==5:
-        if hand != hand[0] * 5:
-            allHands.append(hand + 's')
-        return
-    for card in ranks[ranks.index(hand[-1])+1:]:
-        addFlush(hand + card)
-
-for card in ranks:
-    addNextCard(card)
-    
-for card in ranks:
-    addFlush(card)
-    
-def isStraight(handNoSuit):
-    curr = handNoSuit[0]
-    for c in handNoSuit[1:]:
-        if rankmap[c] != rankmap[curr] + 1:
-            return False
-        curr = c
-    
-    return True
-
-def scoreHand(hand):
-    straight = isStraight(hand[:-1])
-    
-    
-
+ranks = 'AKQJT98765432'  
 
 
 def sortHand(h):
-    return sorted(h, key = lambda w: [ranks.index(c) for c in w])
+    return sorted(h, key = lambda w: [hs.ranks.index(c) for c in w])
     
 
 
-
+@total_ordering
 class Hand:
     def __init__(self, l):
-        if len(l) != 5:
+        if len(l) != HAND_SIZE:
             raise ValueError('Invalid Number of Cards: {}'.format(len(l)))
         self.hand = sortHand([c[0] for c in l])
         self.suits = set([c[1] for c in l])
+        self.cleanedHand = ''.join(self.hand) + ('s' if len(self.suits) == 1 else 'o')
         
+    def __gt__(self, other): 
+        #should we say a hand is greater if the score is less? seems more intuitive
+        return hs.scoreHand(self.cleanedHand) < hs.scoreHand(other.cleanedHand)
+    
+    def __eq__(self, other):
+        return hs.scoreHand(self.cleanedHand) == hs.scoreHand(other.cleanedHand)
 
---
+++ HandScorer.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 28 19:22:20 2018
+
+@author: yuwan
+"""
+
+
+from collections import Counter
+
+HAND_SIZE = 5
+suits = 'cdhs'
+ranks = 'AKQJT98765432'
+rankmap = {r: ranks.index(r) for r in ranks}
+
+handranks = {0: 'Straight Flush',
+             1: 'Four of a Kind',
+             2: 'Full House',
+             3: 'Flush',
+             4: 'Straight',
+             5: 'Three of a Kind',
+             6: 'Two Pair',
+             7: 'One Pair',
+             8: 'High Card'
+             }
+
+cardCountDict = {
+            (1,4): 1,
+            (2,3): 2,
+            (1,1,3): 5,
+            (1,2,2): 6,
+            (1,1,1,2): 7,
+            (1,1,1,1,1): 8
+        }
+
+allHands = []
+temp = [''] * 5
+
+def addNextCard(hand):
+    global allHands
+    if len(hand)==5:
+        if hand != hand[0] * 5:
+            allHands.append(hand + 'o')
+        return
+    for card in ranks[ranks.index(hand[-1]):]:
+        addNextCard(hand + card)
+
+def addFlush(hand):
+    global allHands
+    if len(hand)==5:
+        if hand != hand[0] * 5:
+            allHands.append(hand + 's')
+        return
+    for card in ranks[ranks.index(hand[-1])+1:]:
+        addFlush(hand + card)
+
+for card in ranks:
+    addNextCard(card)
+    
+for card in ranks:
+    addFlush(card)
+    
+def isStraight(handNoSuit):
+    curr = handNoSuit[0]
+    for c in handNoSuit[1:]:
+        if rankmap[c] != rankmap[curr] + 1:
+            return False
+        curr = c
+    
+    return True
+
+def scoreHand(hand):
+    handNoSuit = hand[:-1]
+    straight = isStraight(handNoSuit)
+    flush = hand[-1] == 's'
+    
+    cardCounter = Counter(handNoSuit)
+    sortedHand = sorted(cardCounter.keys(), 
+            key = lambda card: (cardCounter[card],13 -ranks.index(card)),
+            reverse = True
+            )
+    cardCounts = tuple(sorted(cardCounter.values()))
+    
+    if straight and flush:
+        score = [0]
+    elif straight:
+        score = [4]
+    elif flush:
+        score = [3]
+    else:
+        score = [cardCountDict[cardCounts]]
+        
+    score += [ranks.index(card) for card in sortedHand]
+    return score
+                
+allHands = sorted(allHands, key = scoreHand)    
